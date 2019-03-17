bc827be8426db0da22b49c51ce714236751ef546
alok <alok760@gmail.com>
2019-03-17 16:07:17 +0530

    lol1

--- scripts/chroot_reconst.sh
+++ scripts/chroot_reconst.sh
@@ -105,6 +105,7 @@ cat /usr/share/applications/lxqt-about.desktop
 echo "wallpaper exec"
 ls /usr/share/lxqt/themes/meilix
 wget "https://transfer.sh/JjhSu/wallpaper.jpg" -O "/usr/share/lxqt/themes/meilix/wallpaper11.jpg"
+wget "https://transfer.sh/JjhSu/wallpaper.jpg" -O "/usr/share/lxqt/themes/meilix/lol.jpg"
 ls /usr/share/lxqt/themes/meilix
 
 # Switching off screen dimming
